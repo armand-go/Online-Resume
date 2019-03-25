085dd37847ab0385bdd64ba70c79edb24d9e4cdb
armand-gonthier <armdgnthr@gmail.com>
2019-03-25 11:45:52 -0700

    Modified models

--- resumePage/models.py
+++ resumePage/models.py
@@ -11,31 +11,32 @@ class Section(models.Model):
         return self.title
 
 class Bullet(models.Model):
-    section = models.ForeignKey(Section, related_name='bullet' on_delete=models.CASCADE)
+    section = models.ForeignKey(Section, related_name='bullet', on_delete=models.CASCADE)
 
     bulletImage = models.ImageField(default=None, blank=True)
     title = models.CharField(max_length=256, default='')
-    content = models.OneToOneField(Content, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
 
 class Content(models.Model):
-    subtitle = models.CharField(max_length=256, default=None,, blank=True)
+    bullet = models.OneToOneField(Bullet, on_delete=models.CASCADE)
+
+    subtitle = models.CharField(max_length=256, default=None, blank=True)
     subtitleImg = models.ImageField(default=None, blank=True)
 
 class Text(Content):
-    text = models.TextField(default='')
+    description = models.TextField(default='')
 
     def __str__(self):
-        return self.text
+        return self.description
 
 class Bars(Content):
     def __str__(self):
         return self.progress_bar.all()
 
 class ProgressBar(models.Model):
-    bar = models.ForeignKey(Bar, related_name='progress_bar' on_delete=models.CASCADE)
+    bar = models.ForeignKey(Bars, related_name='progress_bar', on_delete=models.CASCADE)
 
     text = models.CharField(max_length=256, default='')
     percentage = models.IntegerField(null=True)
@@ -48,7 +49,7 @@ class Portfolio(Content):
         return self.project.all()
 
 class Project(models.Model):
-    portfolio = models.ForeignKey(Portfolio, related_name='project' on_delete=models.SET_NULL)
+    portfolio = models.ForeignKey(Portfolio, related_name='project', on_delete=models.SET_NULL, null=True)
 
     name = models.CharField(max_length=256, default='')
     illustration = models.ImageField(blank=True, null=True)
