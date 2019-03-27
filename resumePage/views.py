25a7cd923f7958787707d6c1643a9f62deffed36
armand-gonthier <armdgnthr@gmail.com>
2019-03-27 12:26:00 -0700

    Finialized buttons

--- resumePage/views.py
+++ resumePage/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 
 from django.views.generic import TemplateView
+from .models import Introduction
 
 # Create your views here.
-def index(request):
-    return render(request, 'resumePage/index.html')
 
 class ResumeView(TemplateView):
     template_name = 'resumePage/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(TemplateView, self).get_context_data(**kwargs)
+        try:
+            context['intro'] = Introduction.objects.get(pk=1)
+        except Introduction.DoesNotExist:
+            return 'error'
+        return context
+
+    def get(self, request):
+        return render(request, self.template_name, self.get_context_data())
