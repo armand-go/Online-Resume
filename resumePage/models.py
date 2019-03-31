2cd6d0a384ced7e8a1b58e1ed7680d0c4ddd253d
armand-gonthier <armdgnthr@gmail.com>
2019-03-31 09:53:49 -0700

    Modified Content.bullet attribute

--- resumePage/models.py
+++ resumePage/models.py
@@ -61,7 +61,7 @@ class Bullet(models.Model):
 
 class Content(models.Model):
     bullet = models.OneToOneField(Bullet, related_name='%(class)s_content',
-                                on_delete=models.CASCADE, default=None)
+                                on_delete=models.CASCADE, default=None, blank=True)
 
     class Meta:
         abstract = True
